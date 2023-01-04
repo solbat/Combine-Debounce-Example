4c648fb50f0693d6b8f01589d79d9cff37ad72e0
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-04 11:40:23 +0900

    [feat] searchBar 추가

--- CombineDebounceExample/CombineDebounceExample/ViewController.swift
+++ CombineDebounceExample/CombineDebounceExample/ViewController.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myLabel: UILabel!
+    
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController()
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.tintColor = .black
+        searchController.searchBar.searchTextField.accessibilityIdentifier = "mySearchBarTextField"
+        return searchController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
