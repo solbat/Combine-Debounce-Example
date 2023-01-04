38f05809520a3dac1f27106eddf89e948082d74c
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-04 11:41:02 +0900

    [feat] UISearchTextField에 myDebounceSearchPublisher 추가

--- CombineDebounceExample/CombineDebounceExample/ViewController.swift
+++ CombineDebounceExample/CombineDebounceExample/ViewController.swift
@@ -28,3 +28,19 @@ class ViewController: UIViewController {
 
 }
 
+extension UISearchTextField {
+    var myDebounceSearchPublisher: AnyPublisher<String, Never>{
+        NotificationCenter.default.publisher(for: UISearchTextField.textDidChangeNotification, object: self)
+            // NotificationCenter에서 UISearchTextField 가져옴
+            .compactMap { $0.object as? UISearchTextField }
+            // UISearchTextField에서 String 가져오기
+            .map { $0.text ?? "" }
+            // Debounce : 1초 대기
+            .debounce(for: .milliseconds(1000), scheduler: RunLoop.main)
+            // 글자가 있을 때만 이벤트 전달
+            .filter { $0.count > 0 }
+            .print()
+            // 앞서 Publishers.Print<Publishers.CompactMap<NotificationCenter.Publisher, String>>을 AnyPublisher로 퉁침
+            .eraseToAnyPublisher()
+    }
+}
